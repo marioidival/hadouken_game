8c4f5b2ec38a9e87829fd491e7b56ac15aa7d43d
Mario Idival <marioidival@gmail.com>
2014-08-18 11:32:00 -0300

    adicionando respectivos arquivos de testes

---
+++ spec/window_spec.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require_relative '../lib/hadouken_game/window'
+
+describe "GameWindow", "A tela do jogo" do
+
+  it "A tela devera ser do tamanho de 1024x760" do
+  end
+
+  it "A titulo da tela tem que ser 'Hadouken Game'" do
+  end
+
+  it "A tela devera ser instancia de Gosu::Window" do
+  end
+
+end
