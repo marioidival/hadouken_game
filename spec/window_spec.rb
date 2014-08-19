72bed36244396c34fd76710096a5825c387ac73c
Mario Idival <marioidival@gmail.com>
2014-08-18 23:34:01 -0300

    adicionando alguns specs

--- spec/window_spec.rb
+++ spec/window_spec.rb
@@ -1,15 +1,23 @@
 require 'minitest/autorun'
 require_relative '../lib/hadouken_game/window'
 
-describe "GameWindow", "A tela do jogo" do
+describe "GameWindow", "A class da tela do jogo" do
+
+  before do
+    @window = HadoukenGame::GameWindow.new
+  end
 
   it "A tela devera ser do tamanho de 1024x760" do
+    @window.width.must_equal 1024
+    @window.height.must_equal 760
   end
 
   it "A titulo da tela tem que ser 'Hadouken Game'" do
+    @window.caption.must_equal "Hadouken Game"
   end
 
-  it "A tela devera ser instancia de Gosu::Window" do
+  it "A tela nao devera ser fullscreen" do
+    @window.wont_be :==, true
   end
 
 end
