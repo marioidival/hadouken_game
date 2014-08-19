733cb28c635ef94c152daecc5fdcc4270e6f3e9b
Mario Idival <marioidival@gmail.com>
2014-08-18 23:33:46 -0300

    adicionando titulo e tamanho da janela

--- lib/hadouken_game/window.rb
+++ lib/hadouken_game/window.rb
@@ -2,5 +2,11 @@ require 'gosu'
 
 module HadoukenGame
   class GameWindow < Gosu::Window
+    def initialize
+      super 1024, 760, false
+
+      # Titulo da janela
+      self.caption = "Hadouken Game"
+    end
   end
 end
