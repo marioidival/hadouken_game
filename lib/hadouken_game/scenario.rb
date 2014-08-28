178ba97dafa499c59495880af7c5216067fec6eb
Mario Idival <marioidival@gmail.com>
2014-08-28 10:35:20 -0300

    criando classe Scenario, retornando um cenario aleatorio

--- lib/hadouken_game/scenario.rb
+++ lib/hadouken_game/scenario.rb
@@ -1,2 +1,18 @@
 class Scenario
+  attr_accessor :random_background
+  def initialize
+    @scenario = random_background
+  end
+
+  def random_background
+    # Retorna um cenario aleatorio
+    scenarios = [
+      '/media/scenario/image.png',
+      '/media/scenario/image.png',
+      '/media/scenario/image.png',
+      '/media/scenario/image.png',
+    ]
+
+    scenarios[rand(scenarios.size)]
+  end
 end
