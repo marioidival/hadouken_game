f16510ba542cc2aeaf7367fdccd2a4d3c02fb16d
Mario Idival <marioidival@gmail.com>
2014-08-28 12:02:08 -0300

    definindo constantes de tamanhos

--- lib/hadouken_game/window.rb
+++ lib/hadouken_game/window.rb
@@ -2,18 +2,21 @@ require 'gosu'
 require_relative 'scenario'
 
 class GameWindow < Gosu::Window
+  WIDTH = 1024
+  HEIGHT = 771
+
   def initialize
-    super 1024, 760, false
+    super WIDTH, HEIGHT, false
 
     # Titulo da janela
     self.caption = "Hadouken Game"
 
-    @background = Gosu::Image.new self, Scenario.new, true
+    scenario = Scenario.new
+    @background = Gosu::Image.new self, scenario.scenario, true
   end
 
 
   def draw
     @background.draw(0, 0, 0) # Desenhando o cenario
-
   end
 end
