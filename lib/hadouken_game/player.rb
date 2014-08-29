20f7b68efd5f7eae97b47142d805db72fdd97d30
Mario Idival <marioidival@gmail.com>
2014-08-29 07:48:11 -0300

    editando path da imagem

--- lib/hadouken_game/player.rb
+++ lib/hadouken_game/player.rb
@@ -28,16 +28,16 @@ class Player
     # Retorna uma lista com os possiveis personagens do jogo
     # @return Array
     [
-        {name: "italo", hit_value: 5, image: "imagem.png"},
-        {name: "helen", hit_value: 1, image: "media/helen.jpg"},
-        {name: "mateus", hit_value: 5, image: "media/mateus.jpg"},
-        {name: "marisa", hit_value: 1, image: "imagem.png"},
-        {name: "julian", hit_value: false, image: "media/julian.jpg"},
-        {name: "debora", hit_value: 1, image: "media/debora.jpg"},
-        {name: "arisson", hit_value: false, image: "imagem.png"},
-        {name: "eduardo", hit_value: 1, image: "imagem.png"},
-        {name: "luciano", hit_value: 1, image: "media/luciano.jpg"},
-        {name: "wendel", hit_value: 1, image: "noimage.png"},
+   #     {name: "italo", hit_value: 5, image: "imagem.png"},
+        {name: "helen", hit_value: 1, image: "hadouken_game/media/helen.jpg"},
+        {name: "mateus", hit_value: 5, image: "hadouken_game/media/mateus.jpg"},
+   #     {name: "marisa", hit_value: 1, image: "imagem.png"},
+        {name: "julian", hit_value: false, image: "hadouken_game/media/julian.jpg"},
+        {name: "debora", hit_value: 1, image: "hadouken_game/media/debora.jpg"},
+   #     {name: "arisson", hit_value: false, image: "imagem.png"},
+   #     {name: "eduardo", hit_value: 1, image: "imagem.png"},
+        {name: "luciano", hit_value: 1, image: "hadouken_game/media/luciano.jpg"},
+   #     {name: "wendel", hit_value: 1, image: "noimage.png"},
     ]
   end
 
@@ -50,4 +50,5 @@ class Player
   # Gosu Methods
   def draw
   end
+
 end
