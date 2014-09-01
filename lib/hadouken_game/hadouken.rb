7f6ac20fbf3a6a232eb57ad6e027f6cbf8881fe4
Mario Idival <marioidival@gmail.com>
2014-09-01 11:25:55 -0300

    metodo para desenhar o hadouken alterado

--- lib/hadouken_game/hadouken.rb
+++ lib/hadouken_game/hadouken.rb
@@ -6,7 +6,7 @@ class Hadouken
 
     if window
       @window = window
-      @hadouken_img = Gosu::Image.new @window, self.images_hadouken(@player.name), false
+      @hadouken_img = Gosu::Image.new @window, images_hadouken(@player.name), false
     end
   end
 
@@ -15,27 +15,27 @@ class Hadouken
     @center_y = @window.height / 2
     # left
     @hadouken_img.draw(
-      @player.player_x - 10, @center_y, 1, -1
+      @player.player_x - 130, @center_y, 1, -1
     ) if @player.player2?
     # right
     @hadouken_img.draw(
-      @player.player_x + 10, @center_y, 1
+      @player.player_x + 130, @center_y, 1
     ) unless @player.player2?
   end
 
   # Retorna imagem do hadouken do personagem passado por parametro
   def images_hadouken player_name
     images = {
-      helen: 'hadouken_game/media/hadouken/img.jpg',
-      mateus: 'hadouken_game/media/hadouken/img.jpg',
-      julian: 'hadouken_game/media/hadouken/img.jpg',
-      debora: 'hadouken_game/media/hadouken/img.jpg',
-      luciano: 'hadouken_game/media/hadouken/img.jpg',
-      marisa: 'hadouken_game/media/hadouken/img.jpg',
-      arisson: 'hadouken_game/media/hadouken/img.jpg',
-      italo: 'hadouken_game/media/hadouken/img.jpg',
-      eduardo: 'hadouken_game/media/hadouken/img.jpg',
-      wendel: 'hadouken_game/media/hadouken/img.jpg',
+      "helen"=> 'hadouken_game/media/helen.jpg',
+      "mateus"=> 'hadouken_game/media/mateus.jpg',
+      "julian"=> 'hadouken_game/media/julian.jpg',
+      "debora"=> 'hadouken_game/media/debora.jpg',
+      "luciano"=> 'hadouken_game/media/luciano.jpg',
+     # "marisa"=> '',
+     # "arisson"=> '',
+     # "italo"=> '',
+     # "eduardo"=> '',
+     # "wendel"=> '',
     }
     images[player_name]
   end
