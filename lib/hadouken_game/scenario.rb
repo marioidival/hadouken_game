bd8f9974417a53f6b0b0f2e3f350f935a5b29b12
Mario Idival <marioidival@gmail.com>
2014-09-02 09:26:45 -0300

    adicionando novos cenarios na classe

--- lib/hadouken_game/scenario.rb
+++ lib/hadouken_game/scenario.rb
@@ -8,8 +8,18 @@ class Scenario
   def random_background
     # Retorna um cenario aleatorio
     scenarios = [
-      'hadouken_game/media/scenarios/hienstage.jpg',
-      'hadouken_game/media/scenarios/island.png',
+      'hadouken_game/media/scenarios/1.png',
+      'hadouken_game/media/scenarios/2.png',
+      'hadouken_game/media/scenarios/3.png',
+      'hadouken_game/media/scenarios/4.png',
+      'hadouken_game/media/scenarios/5.png',
+      'hadouken_game/media/scenarios/6.png',
+      'hadouken_game/media/scenarios/7.png',
+      'hadouken_game/media/scenarios/8.png',
+      'hadouken_game/media/scenarios/9.png',
+      'hadouken_game/media/scenarios/10.png',
+      'hadouken_game/media/scenarios/11.png',
+      'hadouken_game/media/scenarios/12.png',
     ]
 
     scenarios[rand(scenarios.size)]
