0300ee76b2f5cc22355444b7fed9558ae1488dc2
Mario Idival <marioidival@gmail.com>
2014-09-14 20:13:51 -0300

    mudando valor do hit_value

--- lib/hadouken_game/player.rb
+++ lib/hadouken_game/player.rb
@@ -36,16 +36,16 @@ class Player
     # Retorna uma lista com os possiveis personagens do jogo
     # @return Array
     [
-        {"name"=> "helen", "hit_value" => 1, "image" => "hadouken_game/media/helen.jpg"},
+      {"name"=> "helen", "hit_value" => 3.5, "image" => "hadouken_game/media/helen.jpg"},
         {"name"=> "mateus", "hit_value" => 5, "image" => "hadouken_game/media/mateus.jpg"},
         {"name"=> "julian", "hit_value" => false, "image" => "hadouken_game/media/julian.jpg"},
-        {"name"=> "debora", "hit_value" => 1, "image" => "hadouken_game/media/debora.jpg"},
-        {"name"=> "luciano", "hit_value" => 1, "image" => "hadouken_game/media/luciano.jpg"},
-   #     {"name"=> "marisa", "hit_value" => 1, "image" => "imagem.png"},
+        {"name"=> "debora", "hit_value" => 3.5, "image" => "hadouken_game/media/debora.jpg"},
+        {"name"=> "luciano", "hit_value" => 3.5, "image" => "hadouken_game/media/luciano.jpg"},
+    #     {"name"=> "marisa", "hit_value" => 3.5, "image" => "imagem.png"},
    #     {"name"=> "italo", "hit_value" => 5, "image" => "imagem.png"},
    #     {"name"=> "arisson", "hit_value" => false, "image" => "imagem.png"},
-   #     {"name"=> "eduardo", "hit_value" => 1, "image" => "imagem.png"},
-   #     {"name"=> "wendel", "hit_value" => 1, "image" => "noimage.png"},
+        #     {"name"=> "eduardo", "hit_value" => 3.5, "image" => "imagem.png"},
+        #     {"name"=> "wendel", "hit_value" => 3.5, "image" => "noimage.png"},
     ]
   end
 
